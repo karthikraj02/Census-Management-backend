13ac8d25f534be86a89734ca84de85e817df56ee
Karthik Raj <karthikraj9000@gmail.com>
2026-03-24 13:50:21 +0530

    Initial backend commit

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'Nishamani@#098',
+      database: process.env.DB_NAME || 'vaccination_census',
+    },
+    migrations: {
+      directory: './src/migrations',
+    },
+    seeds: {
+      directory: './src/seeds',
+    },
+  },
+};
